87f50d4db336af16340133b2735226625fc296e3
Zhi Heng <yapzhhg@gmail.com>
2025-02-20 14:54:33 +0800

    rev2: add sqlite3 logging support Finally got it working after 1.5 years💀

--- rev2/README.md
+++ rev2/README.md
@@ -1,5 +1,9 @@
 # Air Quality Dashboard for Raspberry Pi (Revision 2)
 
+## Requirement
+
+- Raspberry Pi OS that is at least `bookworm` (`bullseye` and older versions not supported).
+
 ## Wire connection
 
 // TO ADD LATER
@@ -12,26 +16,16 @@
 
 ## Cross-Compilation from Ubuntu 22.04
 
-Directly compiling for Raspberry Pi OS `bullseye` (which is the latest version of Raspberry Pi OS as of writing this) is currently not supported due to the version of the `libc` linker on the Ubuntu machine being too new - `bullseye` by default does not have the newer `libc` versions. Therefore, it is a requirement to compile for the target `aarch64-unknown-linux-musl` to use static linking. `clang` is used instead of `gcc` due to `gcc` having some issues.
-
-Install the required tools with the following command. 
+Install the required rustup toolchain with the following command.
 
 ```
-sudo apt-get install musl-tools clang llvm -y
-```
-
-To tell `cargo` to use `clang` as the linker, export the following environment values.
-
-```
-export CC_aarch64_unknown_linux_musl=clang
-export AR_aarch64_unknown_linux_musl=llvm-ar
-export CARGO_TARGET_AARCH64_UNKNOWN_LINUX_MUSL_RUSTFLAGS="-Clink-self-contained=yes -Clinker=rust-lld"
+rustup toolchain install stable-aarch64-unknown-linux-gnu --force-non-host
 ```
 
 Finally, compile the project with the following command.
 
 ```
-cargo build --release --target aarch64-unknown-linux-musl
+cargo build --release --target aarch64-unknown-linux-gnu
 ```
 
-The compiled program can be found at `target/aarch64-unknown-linux-musl/release` and can be transferred over to a Raspberry Pi to be executed.
+The compiled program can be found at `target/aarch64-unknown-linux-gnu/release` and can be transferred over to a Raspberry Pi to be executed.
--- rev2/src/main.rs
+++ rev2/src/main.rs
@@ -1,13 +1,38 @@
 mod bme280;
 
 use std::time::Duration;
+use std::thread::sleep;
 
 use bme280::{init_i2c, BmeSensor};
 use mh_z19::{read_gas_concentration, parse_gas_concentration_ppm};
 use rppal::uart::{Uart, Parity};
+use sqlite::{open, Connection};
 use waveshare_rpi::{epd::epd7in5_v2::EPD_CONFIG, Epd};
 use waveshare_rpi::util::{ColorMode, image_to_epd};
 
+const DB_FILE: &str = "data.sqlite3";
+const TIME_PER_READING: u64 = 120;
+
+fn init_db() -> Result<Connection, sqlite::Error> {
+    let conn = Connection::open(DB_FILE)?;
+    let init_query = "CREATE TABLE IF NOT EXISTS air_logs (
+        id INTEGER PRIMARY KEY,
+        co2 INTEGER NOT NULL,
+        temp DOUBLE NOT NULL,
+        humidity DOUBLE NOT NULL,
+        pressure INTEGER NOT NULL,
+        created_at DATETIME DEFAULT (datetime('now', 'localtime')));";
+    conn.execute(init_query)?;
+    Ok(conn)
+}
+
+fn insert_record(db_conn: &Connection, co2: u32, temp: f32, hum: f32, pres: f32) -> Result<(), sqlite::Error> {
+    let insert_query = format!("INSERT INTO air_logs (co2, temp, humidity, pressure) VALUES ({}, {}, {}, {})", co2, temp, hum, pres);
+    db_conn.execute(&insert_query)?;
+    Ok(())
+}
+
+// TODO Make it more fail-safe by returning Result instead
 fn read_mhz19(uart: &mut Uart) -> u32 {
     uart.write(&read_gas_concentration(1)).unwrap();
     let mut buffer: [u8; 9] = [0; 9];
@@ -15,10 +40,20 @@ fn read_mhz19(uart: &mut Uart) -> u32 {
     parse_gas_concentration_ppm(&buffer).unwrap()
 }
 fn main() {
-    let i2c = init_i2c().unwrap();
-    let mut uart = Uart::with_path("/dev/serial0", 9600, Parity::None, 8, 1).unwrap();
-    uart.set_read_mode(9, Duration::default()).unwrap();
-    let bme_reading = i2c.read_bme().unwrap();
-    println!("Temperature: {}°C, Humidity: {}%, Pressure: {}hPa", bme_reading.0, bme_reading.1, bme_reading.2);
-    println!("CO2: {}ppm", read_mhz19(&mut uart));
+    if let Ok(db_conn) = init_db() {
+        let i2c = init_i2c().unwrap();
+        let mut uart = Uart::with_path("/dev/serial0", 9600, Parity::None, 8, 1).unwrap();
+        uart.set_read_mode(9, Duration::default()).unwrap();
+        loop {
+            let bme_reading = i2c.read_bme().unwrap(); // TODO handle err instead of unwrapping
+            let co2 = read_mhz19(&mut uart);
+            insert_record(&db_conn, co2, bme_reading.0, bme_reading.1, bme_reading.2).unwrap();
+            println!("Temperature: {}°C, Humidity: {}%, Pressure: {}hPa", bme_reading.0, bme_reading.1, bme_reading.2);
+            println!("CO2: {}ppm", read_mhz19(&mut uart));
+            sleep(Duration::from_secs(TIME_PER_READING));
+        }
+    } else {
+        println!("Failed to initialize database! Exiting.");
+    }
+    
 }
